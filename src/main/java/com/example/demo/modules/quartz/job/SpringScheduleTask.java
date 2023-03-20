30864db69d0a848b23612b094c4c9787a0bb3483
muzi <900217ai>
2023-03-20 10:32:47 +0800

    定时任务

--- src/main/java/com/example/demo/modules/quartz/job/SpringScheduleTask.java
+++ src/main/java/com/example/demo/modules/quartz/job/SpringScheduleTask.java
@@ -1,10 +1,16 @@
 package com.example.demo.modules.quartz.job;
 
+import com.example.demo.utils.HttpUtils;
+import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicNameValuePair;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.net.URISyntaxException;
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
 
 /**
  * spring自带任务框架，有弊端
@@ -18,6 +24,14 @@ public class SpringScheduleTask {
      */
     @Scheduled(cron = "0/5 * * * * ?")
     public void reptilian(){
+        HttpUtils httpUtils=new HttpUtils();
+        List<NameValuePair> parametersBody = new ArrayList();
+        parametersBody.add(new BasicNameValuePair("userId", "admin"));
+        try {
+            String result = HttpUtils.getRequest("http://www.test.com/user",parametersBody);
+        } catch (URISyntaxException e) {
+            e.printStackTrace();
+        }
         System.out.println("执行调度任务："+new Date());
     }
 }
